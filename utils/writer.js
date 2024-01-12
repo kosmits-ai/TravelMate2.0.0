64bf5e6bbdbc42fafef18c4f8318810bef6d799d
marilita26 <marilitaforou@gmail.com>
2024-01-12 14:48:43 +0200

    push previus code

--- utils/writer.js
+++ utils/writer.js
@@ -1,22 +1,43 @@
-class ResponsePayload {
-  constructor(code = 200, payload = {}) {
-    this.code = code;
-    this.payload = payload;
-  }
+var ResponsePayload = function(code, payload) {
+  this.code = code;
+  this.payload = payload;
 }
 
 exports.respondWithCode = function(code, payload) {
   return new ResponsePayload(code, payload);
 }
 
-exports.writeJson = function(response, arg1, arg2) {
-  let code = arg1 instanceof ResponsePayload ? arg1.code : arg1 || 200;
-  let payload = arg1 instanceof ResponsePayload ? arg1.payload : arg2 || arg1 || {};
+var writeJson = exports.writeJson = function(response, arg1, arg2) {
+  var code;
+  var payload;
 
-  if (typeof payload === 'object') {
-    payload = JSON.stringify(payload, null, 2);
+  if(arg1 && arg1 instanceof ResponsePayload) {
+    writeJson(response, arg1.payload, arg1.code);
+    return;
   }
 
-  response.writeHead(code, { 'Content-Type': 'application/json' });
+  if(arg2 && Number.isInteger(arg2)) {
+    code = arg2;
+  }
+  else {
+    if(arg1 && Number.isInteger(arg1)) {
+      code = arg1;
+    }
+  }
+  if(code && arg1) {
+    payload = arg1;
+  }
+  else if(arg1) {
+    payload = arg1;
+  }
+
+  if(!code) {
+    // if no response code given, we default to 200
+    code = 200;
+  }
+  if(typeof payload === 'object') {
+    payload = JSON.stringify(payload, null, 2);
+  }
+  response.writeHead(code, {'Content-Type': 'application/json'});
   response.end(payload);
-};
+}
